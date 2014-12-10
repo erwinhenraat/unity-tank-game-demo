0e8ea57f78970c08aedace3272a3fff73fc7a6e2
erwin henraat <e.henraat@ma-web.nl>
2014-12-10 21:53:17 +0100

    refactored tank functions

--- Assets/Scripts/Follow.cs
+++ Assets/Scripts/Follow.cs
@@ -4,9 +4,21 @@ using System.Collections;
 public class Follow : MonoBehaviour {
     [SerializeField]
     private Transform target;
+
+    [SerializeField]
+    private float catchup = 0.23f;
+    
+    [SerializeField]
+    private float zoomOutBySpeed = 0f;
+   
+   
+    private BaseTank tankScript;
+    private float startCameraDistance;
+
 	// Use this for initialization
 	void Start () {
-	
+        if (target != null) tankScript = target.gameObject.GetComponent<BaseTank>();
+        startCameraDistance = transform.position.y;
 	}
 	
 	// Update is called once per frame
@@ -15,7 +27,16 @@ public class Follow : MonoBehaviour {
         {
             Vector3 newPos = target.position;
             newPos.y = this.transform.position.y;
-            transform.position = Vector3.MoveTowards(this.transform.position, newPos, 0.23f);
+            transform.position = Vector3.MoveTowards(this.transform.position, newPos, catchup);
         }
+        if (zoomOutBySpeed > 0) 
+        {
+            float speed = Mathf.Abs(tankScript.GetSpeed());                
+            Vector3 pos = transform.position;
+            pos.y = (speed * zoomOutBySpeed + startCameraDistance);
+            transform.position = pos;
+
+        }
+
     }
 }
